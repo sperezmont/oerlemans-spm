db5ff9ae81dc7a85f20ac76191d0566ad96b6a39
sperezmont <sergio.perez.mont@gmail.com>
2022-02-08 09:16:47 +0100

    minor updates

--- temp/test4/libs/bed_profile.py
+++
@@ -1,7 +0,0 @@
-import numpy as numpy
-
-
-def bed_profile(bed_type, r, d0, s):
-    if bed_type == 'linear':
-        d = d0 - s*r
-    return d
--- temp/test4/libs/misc.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def vol2sle(data, rhoi=0.9167, rhow=1.027, A_oc=3.618*10**8):
-    ''' Transforms volume data to m SLE \n
-        [data] = km**3 
-    '''
-    SLE = rhoi/rhow * 1e3 / A_oc * data
-    return SLE
