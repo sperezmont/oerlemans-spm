4cb09951668aae83e9e8cce72f8efca13cd063bd
Sergio Pérez Montero <98036930+sperezmont@users.noreply.github.com>
2022-03-07 09:34:07 +0100

    Update misc.py

--- libs/misc.py
+++ libs/misc.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 
-def vol2sle(data, rhoi=0.9167, rhow=1.027, A_oc=3.618*10**8):
+def vol2sle(data, rhoi=0.9167, rhow=1, A_oc=3.618*10**8):
     ''' Transforms volume data to m SLE \n
         [data] = km**3 
+        rhow = 1 Gt/m3 -> "disregarding the minor salinity/density effects of mixing fresh meltwater with seawater"
+            More about: https://sealevel.info/conversion_factors.html
+        rhow(sea water) = 1.027 Gt/m3
     '''
     SLE = rhoi/rhow * 1e3 / A_oc * data
     return SLE
